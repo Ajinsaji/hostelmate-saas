a3dbf4aca4eef0e96f8ddd552a870a8067d7e93d
Ajin <ajinsaji2711@gmail.com>
2026-05-17 23:15:07 +0530

    Fix duplicate Cloudinary config declarations

--- Backend/config/cloudinary.js
+++ Backend/config/cloudinary.js
@@ -11,13 +11,3 @@ module.exports = {
   cloudinary,
   CloudinaryStorage,
 };
-const cloudinary = require('cloudinary').v2;
-require('dotenv').config();
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
-
-module.exports = cloudinary;
